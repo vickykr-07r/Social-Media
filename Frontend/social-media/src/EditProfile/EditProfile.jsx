8c038f0fed7b59d55cad497f0a7c4f4073827342
vicky kumar <vickykumarpathaldiha7@gmail.com>
2025-12-08 23:17:24 +0530

    a

--- Frontend/social-media/src/EditProfile/EditProfile.jsx
+++ Frontend/social-media/src/EditProfile/EditProfile.jsx
@@ -6,10 +6,13 @@ import { useRef, useState } from "react";
 import { useContext } from "react";
 import { ServerurlContext } from "../Context/Serverurl";
 import axios from "axios"
+import { useNavigate } from "react-router-dom";
+
 function EditProfile(){
     const {userData}=useSelector(state=>state.user)
     let{serverurl}=useContext(ServerurlContext)
-    let[formData,setFormData]=useState({
+    console.log(serverurl)
+    let[forrmData,setFormData]=useState({
       bio:"",
       profession:"",
       gender:""
@@ -17,17 +20,24 @@ function EditProfile(){
     let[image,setImage]=useState(null)
     let[preimage,setpreImage]=useState(null)
     function handleinpt(event){
-     setFormData({...formData,[event.target.name]:event.target.value})
+     setFormData({...forrmData,[event.target.name]:event.target.value})
     }
-    function handleimage(){
-      
-    }
+    function handleimage(event) {
+  const file = event.target.files[0];
+
+  if (file) {
+    setImage(file);  
+    setpreImage(URL.createObjectURL(file)); 
+  }
+}
+
     const ref=useRef();
     async function handlesubmit(){
        const formData = new FormData();
-       formData.append("bio",bio);
-       formData.append("profession",profession);
-       formData.append("gender",gender);
+       formData.append("bio",forrmData.bio);
+       formData.append("profession",forrmData.profession);
+       formData.append("gender",forrmData.gender);
+       formData.append("profileimage",image)
       try {
         let result =await axios.post(`${serverurl}/api/user/editprofile`,formData,{withCredentials:true})
       } catch (error) {
@@ -44,17 +54,17 @@ function EditProfile(){
         </div>
       </div>
 
-     <div className={Style.image} onClick={ref.current.click()}>
-      <img src={image? image : dp} alt="" />
-      <a href="">Change your profile image</a>
+     <div className={Style.image} onClick={()=>{ref.current.click()}}>
+      <img src={preimage? preimage : dp} alt="" />
+      <a href="#">Change your profile image</a>
      </div>
 
      <div className={Style.form}>
      <input type="text" placeholder={userData?.name} readOnly className={Style.a} />
      <input type="text" placeholder={userData?.username} readOnly className={Style.a} />
-     <input type="text" placeholder="Bio" value={bio} onChange={handleinpt} name="bio"/>
-     <input type="text" placeholder="Profession" value={profession} onChange={handleinpt} name="profession"/>
-     <input type="text" placeholder="Gender" value={gender} onChange={handleinpt} name="gender"/>
+     <input type="text" placeholder="Bio" value={forrmData.bio} onChange={handleinpt} name="bio"/>
+     <input type="text" placeholder="Profession" value={forrmData.profession} onChange={handleinpt} name="profession"/>
+     <input type="text" placeholder="Gender" value={forrmData.gender} onChange={handleinpt} name="gender"/>
      <button onClick={handlesubmit}>Save Profile</button>
      </div>
 
